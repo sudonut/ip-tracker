f4f77958c7abcbe3396540cd70a927f6ee6ff7cd
sudonut <paradoxotl@gmail.com>
2021-05-05 21:17:29 -0400

    added some pin functionality when a user enters an IP

--- script/index.js
+++ script/index.js
@@ -2,6 +2,21 @@ const apiKey = "at_RZImwWGNn2H4hohnEiQPF1YpOqygR";
 
 document.getElementById("ip-submit").addEventListener("click", getIp);
 
+let myMap = L.map("map").setView([51.505, -0.09], 13);
+let markerGroup = L.layerGroup().addTo(myMap);
+
+const locationIcon = L.icon({
+  iconUrl: "imgs/icon-location.svg",
+});
+L.tileLayer(
+  "https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=rbPdbXPCW2tS6QkeFTal"
+,
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(myMap);
+
 function getIp() {
   let ip = document.getElementById("ip-input").value;
 
@@ -21,39 +36,12 @@ function getIp() {
       document.getElementById("ip-isp").innerHTML = isp;
 
       console.log(ipAddress, location, timezone);
-    });
+
+      function updateMap() {
+        markerGroup.clearLayers();
+        myMap.panTo({lat: lat, lng: lon});
+        L.marker([lat, lon], {icon: locationIcon}).addTo(markerGroup);
+      }
+      updateMap();
+    })
 }
-
-function generateMap() {
-  let myMap = L.map("map").setView([51.505, -0.09], 13);
-  const locationPin = L.icon({
-    iconUrl: "imgs/icon-location.svg",
-  });
-  L.marker([51.505, -0.09], { icon: locationPin }).addTo(myMap);
-  L.tileLayer(
-    "https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=rbPdbXPCW2tS6QkeFTal"
-  ,
-    {
-      attribution:
-        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    }
-  ).addTo(myMap);
-};
-
-// function generateMap() {
-//   let mymap = L.map("map").setView([51.505, -0.09], 13);
-//   let locationPin = L.icon({
-//     iconUrl: "imgs/icon-location.svg",
-//   });
-//   L.marker([51.5, -0.09], { icon: locationPin }).addTo(mymap);
-
-//   L.tileLayer(
-//     "https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=rbPdbXPCW2tS6QkeFTal",
-//     {
-//       attribution:
-//         '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-//     }
-//   ).addTo(mymap);
-// };
-
-generateMap();
